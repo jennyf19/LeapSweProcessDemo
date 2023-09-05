e7c0b47c506a7ffd803071089bbe904fc17958e6
Douglas Byfield <douglas.byfield@mcsystems.com>
2023-09-05 12:26:49 -0500

    Tried to fix guess feedback messages and add input validation

--- Program.cs
+++ Program.cs
@@ -1,27 +1,38 @@
-﻿namespace LeapSwePracticeApp 
+﻿namespace LeapSwePracticeApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Welcome to the guesing game! I'm thinking of a number between 1 and 100. Can you guess what it is?");
+            Console.Write("Welcome to the guessing game! I'm thinking of a number between 1 and 100. Can you guess what it is?");
             int numberToGuess = new Random().Next(1, 101);
             int guess = 0;
+
             while (guess != numberToGuess)
             {
                 Console.Write("Enter your guess: ");
-                guess = int.Parse(Console.ReadLine());
-                if (guess > numberToGuess)
+
+                // Exception handling for non-integer input
+                try
                 {
-                    Console.Write("Too low! Try again.");
+                    guess = int.Parse(Console.ReadLine());
+
+                    if (guess > numberToGuess)
+                    {
+                        Console.WriteLine("Too high! Try again.");
+                    }
+                    else if (guess < numberToGuess)
+                    {
+                        Console.WriteLine("Too low! Try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Congratulations, you guessed the number!");
+                    }
                 }
-                else if (guess < numberToGuess)
+                catch (FormatException)
                 {
-                    Console.Write("Too high! Try again.");
-                }
-                else
-                {
-                    Console.Write("Congratulations, you guessed the number!");
+                    Console.WriteLine("Please enter a valid number.");
                 }
             }
             Console.ReadLine();
