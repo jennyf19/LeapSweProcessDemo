d0a323d3a1575c7bf191da5b908aca1fde7abc61
Jenny Ferries <jeferrie@microsoft.com>
2025-01-24 20:08:35 -0800

    clean-up and add new project

---
+++ 2025/LeapSweProcessDemo2025/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Welcome to the SWE Process Demo!");
+
+int num1 = 10;
+int num2 = 20;
+int sum = AddNumbers(num1, num2);
+
+Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+
+int AddNumbers(int a, int b)
+{
+    return a * b;
+}
+
+int SubtractNumbers(int a, int b)
+{
+    return a - b;
+}
